a6e90a7d6703c8362805a7fe9c50585989e3abf9
goldfax <nathanield.freed22@gmail.com>
2015-12-22 00:22:09 -0800

    work on map building first

---
+++ src/mapmaker.rb
@@ -0,0 +1,29 @@
+class MapMaker
+
+  attr_accessor :array_h, :array_w
+
+  def get_map_dimensions
+    print "Input map height: " #this is the arrays height
+    @array_h = gets.chomp
+    print "\nInput map width: " #this is the arrays width
+    @array_w = gets.chomp
+  end
+
+  def build_map
+  	@map = Array.new(@width) { Array.new(@height) }
+  end
+
+  def build_rmap input_w, input_h
+  	random_n = Random.new()
+	@r_height = random_n.rand(0..input_h)
+	@r_width = random_n.rand(0..input_w)
+	@map = Array.new(@r_width) { Array.new(@r_height) }
+  end
+
+=begin
+
+  each index in the 2d array will denote a space in the game
+  in each index there will be another array that will contain values that
+  describe that space in the game. for example 
+  [terrain_type,  
+
