31c6edd95aa8fc60a374ad494e33fdb480645757
goldfax <nathanield.freed22@gmail.com>
2015-12-28 21:49:52 -0800

    still testing monster and mapmaker classes

--- src/test.rb
+++ src/test.rb
@@ -3,10 +3,8 @@ require './monster.rb'
 
 
 map2 = MapMaker.new(7,7)
-for i in 0..map2.array_w - 1
-  for j in 0..map2.array_h - 1
-    map2.set_map_value(i, j, [0, 0, 0])
-  end
-end
+dragon1 = Dragon.new("Mjolnir")
+map2.set_map_value 2, 3, [dragon1, 0, 0] #here make sure the first one is the monster
 map2.print_map
+
 
