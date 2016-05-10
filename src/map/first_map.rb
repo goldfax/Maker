43f149c6a545ecb7cce62564d9ffa2d8ec5fcd10
goldfax <nathanield.freed22@gmail.com>
2016-05-09 22:57:54 -0700

    Made some progress on the mapmaker

--- src/map/first_map.rb
+++ src/map/first_map.rb
@@ -1,2 +1,9 @@
-require './map_node.rb'
+require './mapmaker.rb'
 
+_map = MapMaker.new(100, 100, 20)
+_map.map[0][0] = Room.new('nw',{monster: 1, player: 1, item: 1, maptype: 1})
+_map.map[0][0].n = EmptyRoom.new() 
+puts _map.map[0][0].nw
+puts _map.map[0][0].n
+puts _map.map[0][0].w
+puts _map.map[0][0].value[]
--- src/map/mapmaker.rb
+++ src/map/mapmaker.rb
@@ -4,20 +4,23 @@
 #need a number to decide whether to make just north, just south, just west, just east
 #or to make two rooms or three rooms or four rooms for on
 
-require 'map_node.rb'
+require './map_node.rb'
 
 class MapMaker
   #randomize dimensions for map
+  attr_accessor :map
   def initialize x, y, limit #parameter to limit the number of rooms
     rand_num = Random.new()
     @x = x    
     @y = y  
     @map = Array.new(@x){Array.new(@y)} #initalize randomly large
-    loop do  #generate random coordinates
-      new_num_x = rand_num.rand(0..@x)
-      new_num_y = rand_num.rand(0..@y)
-                                     
-    end                           
+    @limit = limit
+    @count = 0
+    #loop do  #generate random coordinates
+     # new_num_x = rand_num.rand(0..@x)
+      #new_num_y = rand_num.rand(0..@y)
+      #break if count >= limit
+    #end                                                       
   end                            
 
 end
